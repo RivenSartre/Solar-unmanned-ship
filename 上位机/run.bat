315fdb752e689573c772508dee9b09bf1b020878
RivenSartre <74661424+RivenSartre@users.noreply.github.com>
2024-06-19 20:13:10 +0800

    Add files via upload

---
+++ 上位机/run.bat
@@ -0,0 +1,4 @@
+@echo off
+call conda activate hus
+python main.py -t 0
+pause
---
+++ 上位机/tmp.py
@@ -0,0 +1,32 @@
+{
+            0x01: {
+                # "pv_input_voltage": [34.913, 34.995, 35.013, 34.989, 34.981, 34.967],
+                # "pv_input_current": [1.32, 1.31, 0, 0, 0, 0],
+                # "pv_output_voltage": [25.2, 24.95, 24.7, 43.1, 43.5, 43.5],
+                # "pv_output_current": [1.83, 1.83, 0, 0, 0, 0],
+                "pv_input_voltage": [35.3],
+                "pv_input_current": [1.318],
+                "pv_output_voltage": [23.07],
+                "pv_output_current": [2.018],
+            },
+            0x02: {
+                # "dcdc1_input_voltage": [25.2, 24.95, 24.7, 23.5, 22.3, 21.1],
+                # "dcdc1_input_current": [5.30, 5.34, 5.53, 5.83, 6.11, 6.50],
+                # "dcdc1_output_voltage": [11.97, 11.96, 12.05, 11.91, 11.98, 12.08],
+                # "dcdc1_output_current": [10.58, 10.57, 10.43, 10.60, 10.46, 10.44]
+                "dcdc1_input_voltage": [23.07],
+                "dcdc1_input_current": [2.92/2],
+                "dcdc1_output_voltage": [11.97],
+                "dcdc1_output_current": [2.92]
+            },
+            0x03: {
+                "dcdc2_input_voltage": [0],
+                "dcdc2_input_current": [0],
+                "dcdc2_output_voltage": [0],
+                "dcdc2_output_current": [0]
+            },
+            0x04: {
+                "battery_voltage": [23.07],
+                "battery_soc": [0]
+            }
+        }
